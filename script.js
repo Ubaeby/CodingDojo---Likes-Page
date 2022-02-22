14a78fd66c4b481081df1772fc4c4133011aef6c
Ubaeby <christianchoi45@gmail.com>
2022-02-22 13:55:32 -0800

    Finished Assignment for Likes

---
+++ script.js
@@ -0,0 +1,25 @@
+let like1 = document.querySelector(".like");
+let count1 = 0;
+function likeOne() {
+    like1.textContent = `${count1} like(s)`;
+    count1 ++;
+} likeOne();
+
+
+let like2 = document.querySelector(".like2");
+let count2 = 0;
+function likeTwo() {
+    like2.textContent = `${count2} like(s)`;
+    count2 ++;
+} likeTwo();
+
+
+let like3 = document.querySelector(".like3");
+let count3 = 0;
+function likeThree() {
+    like3.textContent = `${count3} like(s)`;
+    count3 ++;
+} likeThree();
+
+
+
